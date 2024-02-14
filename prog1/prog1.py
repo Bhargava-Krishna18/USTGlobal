24d2fbd2b88fd019b0ea3b209b6d127b8f94ac86
Bhagi <bhargavdependable@gmail.com>
2024-02-14 12:04:17 +0530

    prog1 checkin

--- prog1/prog1.py
+++ prog1/prog1.py
@@ -1,1 +1,9 @@
-print("hi")
+def divisible_by_5_and_7_generator(n):
+    for num in range(n + 1):
+        if num % 5 == 0 and num % 7 == 0:
+            yield num
+            
+            
+given_number = 100
+result = ','.join(str(num) for num in divisible_by_5_and_7_generator(given_number))
+print(result)
